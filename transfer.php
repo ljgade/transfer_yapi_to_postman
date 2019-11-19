c2a7dcdda9c0948c22b381f1406a359b3024c5a2
ljgade <ljgade@qq.com>
2019-11-19 17:48:21 +0800

    update

--- transfer.php
+++ transfer.php
@@ -1,10 +1,6 @@
 <?php
 include_once 'utils.php';
 
-//$data = json_decode("{\"\$schema\":\"http://json-schema.org/draft-04/schema#\",\"type\":\"object\",\"properties\":{\"file_id\":{\"type\":\"string\",\"description\":\"文件id\"},\"file_name\":{\"type\":\"string\",\"description\":\"文件名\"},\"refer_design_code\":{\"type\":\"string\",\"description\":\"参考版型号\"},\"refer_design_code_flag\":{\"type\":\"string\",\"description\":\"是否参考版型：1-是，2-否\"},\"from_act\":{\"type\":\"string\",\"description\":\"来源操作页面：paper-纸样进行中，paper-lib纸样库，paper-confirm纸样修改，size-confirm尺寸推码维护\"},\"design_code\":{\"type\":\"string\",\"description\":\"设计款号\"},\"version_code\":{\"type\":\"string\",\"description\":\"样衣版本号，归档列表时需要传\"}},\"required\":[\"file_id\",\"from_act\",\"refer_design_code_flag\",\"file_name\"]}");
-//$result = parseSchema($data);
-//header("Content-Type: application/json");
-//exit(json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 $ext = 'json';
 $uploadFile = $_FILES['uploadFile'] ?? [];
 if (strpos($uploadFile['name'], $ext) != (strlen($uploadFile['name']) - 4)) {
